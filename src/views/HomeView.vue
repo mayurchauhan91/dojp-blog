f9247afc10142c0dd3538e13552133ea9da68daf
Mayur_Chauhan <mayurchauhan@live.com>
2022-10-10 11:33:49 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home"></div>
+  <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+  <button @click="handleClick">Click me</button>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+
+export default {
+  name: "HomeView",
+  setup() {
+    const p = ref(null);
+
+    let name = "Mayur";
+    let age = 31;
+    const handleClick = () => {
+      console.log(p, p.value);
+      p.value.classList.add("test");
+    };
+
+    return { name, age, handleClick, p };
+  },
+};
+</script>
